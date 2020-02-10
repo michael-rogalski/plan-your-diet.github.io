042aad7487c61c095f4213860b78ee8582b443e8
Michał Rogalski <mrogalski.graf@gmail.com>
2020-02-10 22:31:19 +0100

    zmiana katalogu i dodanie delpya do gulpa

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,27 @@ var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
 var autoprefixer = require("gulp-autoprefixer");
 var browserSync = require("browser-sync").create();
+var deploy      = require('gulp-gh-pages');
 
 gulp.task("watch", function(cb) {
-  gulp.watch("development/scss/**/*.scss", gulp.series("sass"));
+  gulp.watch("dist/scss/**/*.scss", gulp.series("sass"));
+  gulp.watch("src/scss/**/*.scss", gulp.series("sass"));
+  gulp.watch("dist/*.html").on("change", browserSync.reload);
+  gulp.watch("dist/js/*.js").on("change", browserSync.reload);
   cb();
 });
 
 gulp.task("serve", function(cb) {
   browserSync.init({
-    server: "./development"
+    server: "./dist"
   });
-  gulp.watch("development/scss/**/*.scss", gulp.series("sass"));
-  gulp.watch("development/*.html").on("change", browserSync.reload);
-  gulp.watch("development/js/*.js").on("change", browserSync.reload);
   cb();
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task("sass", function() {
   return gulp
-    .src("development/scss/**/*.scss")
+    .src("src/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(sourcemaps.init())
     .pipe(
@@ -31,8 +32,15 @@ gulp.task("sass", function() {
       })
     )
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest("development/css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 });
+
+
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
 
 gulp.task("default", gulp.series("serve"));
